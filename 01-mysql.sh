795bee93b4de1ad009c56273c2fe9a2f4b2bb835
srikanth24831 <srinukatteboina16@gmail.com>
2025-04-18 23:43:34 +0530

    shell

--- 01-mysql.sh
+++ 01-mysql.sh
@@ -15,28 +15,30 @@ Y=\e[33"
 CHECK_ROOT(){
   if [ $USERID -ne 0 ]
 then
-   echo -e "$R please run this script with root privilages $N" &>>$LOG_FILE
+   echo -e "$R please run this script with root privilages $N" | tee -a $LOG_FILE 
      exit 1
-  fi
+ fi
 }
 VALIDATE(){
 if [ $1 -ne 0 ]
 then
-     echo -e "$2 is...$R SUCCESS $N" &>>$LOG_FILE
+     echo -e "$2 is...$R FAILED $N" | tee -a $LOG_FILE
      exit 1
 else
-     echo -e "$2 is...$G FAILED $N" &>>$LOG_FILE
+     echo -e "$2 is...$G SUCCESS $N" | tee -a $LOG_FILE
 fi
 
 }
 echo "script started executing at: $(date)" | tee -a $LOG_FILE
+
 CHECK_ROOT
 
 dnf instal mysql-sever -y
 VALIDATE $? "installing mysql server"
 
 systemctl enable mysqld
-VALIDATE $? "enabled mySQL server"
+VALIDATE $? "Enabled mySQL server"
+
 systemctl start mysqld
 VALIDATE $? started mySQL server
 
