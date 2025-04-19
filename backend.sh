61baba5f7af75ed265cf2b517fad8ec3827d93c5
srikanth24831 <srinukatteboina16@gmail.com>
2025-04-19 22:44:44 +0530

    shell

---
+++ backend.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILES="$LOGS_FOLDER/SCRIPT_NAME-$TIMESTAMP.log"
+mkdir -p $LOGS_FOLDER
+
+USERID=$(id -u)
+R="\e[31m"
+G=\e[32m"
+N=\e[0m"
+Y=\e[33"
+
+CHECK_ROOT(){
+  if [ $USERID -ne 0 ]
+then
+   echo -e "$R please run this script with root privilages $N" | tee -a $LOG_FILE 
+     exit 1
+ fi
+}
+VALIDATE(){
+if [ $1 -ne 0 ]
+then
+     echo -e "$2 is...$R FAILED $N" | tee -a $LOG_FILE
+     exit 1
+else
+     echo -e "$2 is...$G SUCCESS $N" | tee -a $LOG_FILE
+fi
+
+}
+echo "script started executing at: $(date)" | tee -a $LOG_FILE
+
+CHECK_ROOT
+
